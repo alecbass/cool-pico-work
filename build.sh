d2d198f4356bc7dd5569ee3db5da9c524118db07
alecbass <alec.bassingthwaighte@gmail.com>
2023-01-22 17:15:09 +1000

    Change build.sh location.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 TARGET=target/thumbv6m-none-eabi/release/cool-pico-work
-OUT=/mnt/c/Users/alecb/Documents/out.uf2
+OUT=/mnt/c/Users/alecb/Documents/Programming/Hardware/out.uf2
 
 cargo build --release && echo "Built!"
 elf2uf2-rs "$TARGET" "$OUT"
