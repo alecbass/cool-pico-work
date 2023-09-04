dd03caa795fa366e372093052bd0dab5616f7ca7
alecbass <alec.bassingthwaighte@gmail.com>
2023-09-04 04:58:33 +0000

    Working async functions!!!!

--- src/time/driver.rs
+++ src/time/driver.rs
@@ -1,21 +1,25 @@
 use embassy_time::driver::{AlarmHandle, Driver};
 
-struct MyDriver {}
+struct PicoDriver {}
 
-embassy_time::time_driver_impl!(static DRIVER: MyDriver = MyDriver {});
+embassy_time::time_driver_impl!(static DRIVER: PicoDriver = PicoDriver {});
 
-impl Driver for MyDriver {
+// NOTE(alec): I have absolutely no idea what I'm doing with this
+impl Driver for PicoDriver {
     fn now(&self) -> u64 {
-        todo!()
+        // Around 2:57p.m AEST 04/09/2023
+        1693803406
     }
 
     unsafe fn allocate_alarm(&self) -> Option<AlarmHandle> {
-        todo!()
+        Some(AlarmHandle::new(1))
     }
+
     fn set_alarm_callback(&self, alarm: AlarmHandle, callback: fn(*mut ()), ctx: *mut ()) {
-        todo!()
+        callback(ctx);
     }
+
     fn set_alarm(&self, alarm: AlarmHandle, timestamp: u64) -> bool {
-        todo!()
+        true
     }
 }
