8db2f4d62efe348e2623a9727fbc89068af8f6ad
alecbass <alec.bassingthwaighte@gmail.com>
2023-09-10 08:29:49 +0000

    Overriden linker errors, now getting .boot2 memory warnings.

---
+++ interrupts/src/lib.rs
@@ -0,0 +1,19 @@
+//! UNified library with RP2040 interrupts
+#![no_std]
+
+mod _vectors;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
