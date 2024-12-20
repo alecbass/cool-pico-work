a26777bee43a4f33782d43da1484bfc4bcdc754c
alecbass <alec.bassingthwaighte@gmail.com>
2024-12-20 16:18:20 +1100

    Working servo upon button press!

--- cyw43-firmware/README.md
+++
@@ -1,3 +0,0 @@
-# Copied from https://github.com/embassy-rs/embassy/tree/main/cyw43-firmware
-
-# This should probably be synced every once in a while
--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,8 @@ use embedded_graphics::pixelcolor::BinaryColor;
 use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::{PrimitiveStyleBuilder, Rectangle};
 use embedded_graphics::text::{Alignment, Text};
-use embedded_hal::digital::OutputPin;
+use embedded_hal::digital::InputPin;
+use embedded_hal::pwm::SetDutyCycle;
 use fugit::RateExtU32;
 use panic_probe as _;
 
@@ -25,16 +26,17 @@ use rp_pico as bsp;
 // use sparkfun_pro_micro_rp2040 as bsp;
 
 use bsp::entry;
+use bsp::hal::clocks::{init_clocks_and_plls, Clock};
 use bsp::hal::gpio::{FunctionI2C, FunctionUart, PullNone, PullUp};
 use bsp::hal::i2c::I2C;
+use bsp::hal::pac;
+use bsp::hal::pwm::Slices;
+use bsp::hal::sio::Sio;
 use bsp::hal::uart::{DataBits, StopBits, UartConfig, UartPeripheral};
-use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac,
-    sio::Sio,
-    watchdog::Watchdog,
-};
+use bsp::hal::watchdog::Watchdog;
+use bsp::hal::Timer;
 use bsp::Pins;
+use servo::Servo;
 use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 
 mod i2c;
@@ -44,6 +46,7 @@ mod piicodev_qmc6310;
 mod piicodev_rgb;
 mod piicodev_ssd1306;
 mod piicodev_vl53l1x;
+mod servo;
 mod uart;
 
 use i2c::I2CHandler;
@@ -126,16 +129,47 @@ fn main() -> ! {
         )
         .unwrap();
 
-    let mut led = pins.led.into_push_pull_output();
-    led.set_high().unwrap();
+    // let connection_attempt = unsafe { connectToWifi() };
 
-    writeln!(uart, "hieeeeGGGGeee").unwrap();
+    // Init PWMs
+    let mut pwm_slices = Slices::new(pac.PWM, &mut pac.RESETS);
 
-    let connection_attempt = unsafe { connectToWifi() };
+    // Configure PWM0
+    let pwm = &mut pwm_slices.pwm0;
+    pwm.set_ph_correct();
+    pwm.set_div_int(20u8); // 50 hz
+    pwm.enable();
 
+    // Reset GPIO15 for a button to read input
+    let mut button_pin = pins.gpio15.into_pull_down_input();
+
+    // Output channel B on PWM0 to the GPIO17 pin
+    let channel = &mut pwm.channel_b;
+    channel.output_to(pins.gpio17);
+
+    let mut servo = Servo::new(channel);
+
+    // Reset the servo
+    writeln!(uart, "Resetting the servo").unwrap();
+    servo.set_minimum_angle().unwrap();
+
+    // Infinite loop, moving micro servo from one position to another.
+    // You may need to adjust the pulse width since several servos from
+    // different manufacturers respond differently.
     loop {
-        writeln!(uart, "Continuing... {}", connection_attempt).unwrap();
-        delay.delay_ms(1000);
+        writeln!(uart, "Continuing...").unwrap();
+
+        let is_button_pressed = button_pin.is_high().unwrap_or(false);
+
+        if is_button_pressed {
+            // Move the servo
+            servo.set_maximum_angle().unwrap();
+            delay.delay_ms(2000);
+        } else {
+            servo.set_minimum_angle().unwrap();
+        }
+
+        delay.delay_ms(50);
     }
 
     // Write something to the UART on start-up so we can check the output pin
@@ -150,236 +184,236 @@ fn main() -> ! {
         125_000_000.Hz(),
     );
 
-    let declination = 12.64; // Brisbane
-    let mut magnetometer = PiicoDevQMC6310::new(None, Some(GaussRange::Gauss1200), declination);
-
-    if magnetometer.init(&mut i2c).is_err() {
-        writeln!(uart, "Failed to initialise magnetometer").unwrap();
-    }
-    delay.delay_ms(5);
-
-    delay.delay_ms(1000);
-
-    // if magnetometer
-    //     .calibrate(false, &mut i2c, &mut uart, &mut delay)
-    //     .is_err()
-    // {
-    //     writeln!(uart, "Failed to calibrate magnetometer").unwrap();
+    // let declination = 12.64; // Brisbane
+    // let mut magnetometer = PiicoDevQMC6310::new(None, Some(GaussRange::Gauss1200), declination);
+    //
+    // if magnetometer.init(&mut i2c).is_err() {
+    //     writeln!(uart, "Failed to initialise magnetometer").unwrap();
     // }
-
-    loop {
-        let reading = magnetometer.read_polar(&mut i2c, &mut uart);
-
-        if let Ok(reading) = reading {
-            writeln!(uart, "Polar: {}°", reading.polar as u16).unwrap();
-        } else {
-            writeln!(uart, "Failed to read magnetometer").unwrap();
-        }
-
-        delay.delay_ms(100);
-    }
-
-    let interface = I2CDisplayInterface::new(i2c);
-
-    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
-        .into_buffered_graphics_mode();
-
-    display.init().unwrap();
-
-    // NOTE: SSD1306 only supports binary colours: on and off (white and black)
-    let white_text_style = MonoTextStyleBuilder::new()
-        .font(&FONT_6X10)
-        .text_color(BinaryColor::On)
-        .build();
-
-    let black_text_style = MonoTextStyleBuilder::new()
-        .font(&FONT_6X10)
-        .text_color(BinaryColor::Off)
-        .build();
-
-    let white_rectangle_style = PrimitiveStyleBuilder::new()
-        .fill_color(BinaryColor::On)
-        .stroke_color(BinaryColor::On)
-        .stroke_width(3)
-        .build();
-
-    Rectangle::new(Point::zero(), Size::new(128, 64))
-        .into_styled(white_rectangle_style)
-        .draw(&mut display)
-        .unwrap();
-
-    Text::with_alignment(
-        "80% of boys have",
-        Point::new(64, 8),
-        black_text_style,
-        Alignment::Center,
-    )
-    .draw(&mut display)
-    .unwrap();
-
-    Text::with_alignment(
-        "girlfriends",
-        Point::new(64, 24),
-        black_text_style,
-        Alignment::Center,
-    )
-    .draw(&mut display)
-    .unwrap();
-
-    Text::with_alignment(
-        "rest 20% are having",
-        Point::new(64, 40),
-        black_text_style,
-        Alignment::Center,
-    )
-    .draw(&mut display)
-    .unwrap();
-
-    Text::with_alignment(
-        "a brain",
-        Point::new(64, 56),
-        black_text_style,
-        Alignment::Center,
-    )
-    .draw(&mut display)
-    .unwrap();
-
-    display.flush().unwrap();
-
-    // Turn IO devices into shared pointers
-    let i2c_cell = RefCell::new(i2c);
-    let uart_cell = RefCell::new(uart);
-    let delay_cell = RefCell::new(delay);
-
-    let mut distance_sensor = PiicoDevVL53L1X::new(None, &i2c_cell, &delay_cell);
-    distance_sensor.init().unwrap();
-
-    // Set up the RGB device
-    let mut rgb = PiicoDevRGB::new(&i2c_cell);
-
-    // Turn the LED on
-    rgb.power_led(true).unwrap();
-
-    // Create the buzzer
-    let mut buzzer = PiicoDevBuzzer::new(&i2c_cell, &delay_cell);
-
-    // Initialise the buzzer
-    buzzer.init().unwrap();
-    buzzer.volume(BuzzerVolume::Low).unwrap();
-
-    // Initialise the temperature sensor
-    let mut temperature_sensor = PiicoDevBME280::new(&i2c_cell, &delay_cell);
-    temperature_sensor.init().unwrap();
-
-    // Increases evey time the sensor reads close OR reads far consecutively
-    let mut last_is_close = false;
-    let mut same_reading_index = 0;
-
-    // How long to wait until the next reading
-    const NEAR_DELAY: u32 = 20;
-    const FAR_DELAY: u32 = 1000;
-
-    let mut next_delay;
-
-    const COLOUR_MAP: [(u8, u8, u8); 6] = [
-        (255, 0, 0),
-        (0, 255, 0),
-        (0, 0, 255),
-        (255, 255, 0),
-        (0, 255, 255),
-        (255, 0, 255),
-    ];
-
-    // Play repeated song
-    let mut note_index = 0;
-    let song = HARMONY;
-
-    // Set up the OLED display
-    let mut oled = PiicoDevSSD1306::new(&i2c_cell);
-    oled.init().unwrap();
-    oled.fill(OLEDColour::WHITE);
-
-    for i in 64..0 {
-        oled.pixel(i, i, OLEDColour::BLACK);
-    }
-
-    oled.show().unwrap();
-
-    loop {
-        let reading = distance_sensor.read().unwrap();
-
-        let mut uart = uart_cell.borrow_mut();
-
-        let is_close = reading < 100;
-
-        // Have we gone from close to near, or from near to close?
-        let did_change = is_close != last_is_close;
-
-        if did_change {
-            same_reading_index = 0;
-        } else {
-            same_reading_index += 1;
-        }
-
-        last_is_close = is_close;
-
-        if is_close {
-            writeln!(uart, "GAMERS DETECTED!!!!! JULIA, KAFFY, SCRYBID").unwrap();
-            // Set brightness
-            rgb.set_brightness(20).unwrap();
-
-            let left = COLOUR_MAP[same_reading_index % 6];
-            let middle = COLOUR_MAP[(same_reading_index + 1) % 6];
-            let right = COLOUR_MAP[(same_reading_index + 2) % 6];
-
-            // Set colours
-            rgb.set_pixel(0, left);
-            rgb.set_pixel(1, middle);
-            rgb.set_pixel(2, right);
-
-            next_delay = NEAR_DELAY;
-
-            // Play the next note of the song
-            let (note, duration) = HARMONY[note_index];
-            // buzzer.tone(&note, duration).unwrap();
-
-            note_index += 1;
-
-            let is_at_end_of_song = note_index >= song.len();
-
-            if is_at_end_of_song {
-                // Restart the song
-                note_index = 0;
-            }
-        } else {
-            writeln!(uart, "No gaming detected in the vicinity...").unwrap();
-            // Set brightness
-            rgb.set_brightness(5).unwrap();
-
-            // Set colours
-            rgb.set_pixel(0, (255, 0, 0));
-            rgb.set_pixel(1, (0, 255, 0));
-            rgb.set_pixel(2, (0, 0, 255));
-
-            next_delay = FAR_DELAY;
-        }
-
-        rgb.show().unwrap();
-
-        let readings = temperature_sensor.values().unwrap();
-        let altitude = temperature_sensor.altitude(None).unwrap();
-        writeln!(
-            uart,
-            "Temperature: {} Pressure: {} Humidity: {} Altitude: {}",
-            readings.temperature, readings.pressure, readings.humidity, altitude
-        )
-        .unwrap();
-
-        {
-            let mut delay = delay_cell.borrow_mut();
-            delay.delay_ms(next_delay);
-        }
-    }
+    // delay.delay_ms(5);
+    //
+    // delay.delay_ms(1000);
+    //
+    // // if magnetometer
+    // //     .calibrate(false, &mut i2c, &mut uart, &mut delay)
+    // //     .is_err()
+    // // {
+    // //     writeln!(uart, "Failed to calibrate magnetometer").unwrap();
+    // // }
+    //
+    // loop {
+    //     let reading = magnetometer.read_polar(&mut i2c, &mut uart);
+    //
+    //     if let Ok(reading) = reading {
+    //         writeln!(uart, "Polar: {}°", reading.polar as u16).unwrap();
+    //     } else {
+    //         writeln!(uart, "Failed to read magnetometer").unwrap();
+    //     }
+    //
+    //     delay.delay_ms(100);
+    // }
+    //
+    // let interface = I2CDisplayInterface::new(i2c);
+    //
+    // let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+    //     .into_buffered_graphics_mode();
+    //
+    // display.init().unwrap();
+    //
+    // // NOTE: SSD1306 only supports binary colours: on and off (white and black)
+    // let white_text_style = MonoTextStyleBuilder::new()
+    //     .font(&FONT_6X10)
+    //     .text_color(BinaryColor::On)
+    //     .build();
+    //
+    // let black_text_style = MonoTextStyleBuilder::new()
+    //     .font(&FONT_6X10)
+    //     .text_color(BinaryColor::Off)
+    //     .build();
+    //
+    // let white_rectangle_style = PrimitiveStyleBuilder::new()
+    //     .fill_color(BinaryColor::On)
+    //     .stroke_color(BinaryColor::On)
+    //     .stroke_width(3)
+    //     .build();
+    //
+    // Rectangle::new(Point::zero(), Size::new(128, 64))
+    //     .into_styled(white_rectangle_style)
+    //     .draw(&mut display)
+    //     .unwrap();
+    //
+    // Text::with_alignment(
+    //     "80% of boys have",
+    //     Point::new(64, 8),
+    //     black_text_style,
+    //     Alignment::Center,
+    // )
+    // .draw(&mut display)
+    // .unwrap();
+    //
+    // Text::with_alignment(
+    //     "girlfriends",
+    //     Point::new(64, 24),
+    //     black_text_style,
+    //     Alignment::Center,
+    // )
+    // .draw(&mut display)
+    // .unwrap();
+    //
+    // Text::with_alignment(
+    //     "rest 20% are having",
+    //     Point::new(64, 40),
+    //     black_text_style,
+    //     Alignment::Center,
+    // )
+    // .draw(&mut display)
+    // .unwrap();
+    //
+    // Text::with_alignment(
+    //     "a brain",
+    //     Point::new(64, 56),
+    //     black_text_style,
+    //     Alignment::Center,
+    // )
+    // .draw(&mut display)
+    // .unwrap();
+    //
+    // display.flush().unwrap();
+    //
+    // // Turn IO devices into shared pointers
+    // let i2c_cell = RefCell::new(i2c);
+    // let uart_cell = RefCell::new(uart);
+    // let delay_cell = RefCell::new(delay);
+    //
+    // let mut distance_sensor = PiicoDevVL53L1X::new(None, &i2c_cell, &delay_cell);
+    // distance_sensor.init().unwrap();
+    //
+    // // Set up the RGB device
+    // let mut rgb = PiicoDevRGB::new(&i2c_cell);
+    //
+    // // Turn the LED on
+    // rgb.power_led(true).unwrap();
+    //
+    // // Create the buzzer
+    // let mut buzzer = PiicoDevBuzzer::new(&i2c_cell, &delay_cell);
+    //
+    // // Initialise the buzzer
+    // buzzer.init().unwrap();
+    // buzzer.volume(BuzzerVolume::Low).unwrap();
+    //
+    // // Initialise the temperature sensor
+    // let mut temperature_sensor = PiicoDevBME280::new(&i2c_cell, &delay_cell);
+    // temperature_sensor.init().unwrap();
+    //
+    // // Increases evey time the sensor reads close OR reads far consecutively
+    // let mut last_is_close = false;
+    // let mut same_reading_index = 0;
+    //
+    // // How long to wait until the next reading
+    // const NEAR_DELAY: u32 = 20;
+    // const FAR_DELAY: u32 = 1000;
+    //
+    // let mut next_delay;
+    //
+    // const COLOUR_MAP: [(u8, u8, u8); 6] = [
+    //     (255, 0, 0),
+    //     (0, 255, 0),
+    //     (0, 0, 255),
+    //     (255, 255, 0),
+    //     (0, 255, 255),
+    //     (255, 0, 255),
+    // ];
+    //
+    // // Play repeated song
+    // let mut note_index = 0;
+    // let song = HARMONY;
+    //
+    // // Set up the OLED display
+    // let mut oled = PiicoDevSSD1306::new(&i2c_cell);
+    // oled.init().unwrap();
+    // oled.fill(OLEDColour::WHITE);
+    //
+    // for i in 64..0 {
+    //     oled.pixel(i, i, OLEDColour::BLACK);
+    // }
+    //
+    // oled.show().unwrap();
+    //
+    // loop {
+    //     let reading = distance_sensor.read().unwrap();
+    //
+    //     let mut uart = uart_cell.borrow_mut();
+    //
+    //     let is_close = reading < 100;
+    //
+    //     // Have we gone from close to near, or from near to close?
+    //     let did_change = is_close != last_is_close;
+    //
+    //     if did_change {
+    //         same_reading_index = 0;
+    //     } else {
+    //         same_reading_index += 1;
+    //     }
+    //
+    //     last_is_close = is_close;
+    //
+    //     if is_close {
+    //         writeln!(uart, "GAMERS DETECTED!!!!! JULIA, KAFFY, SCRYBID").unwrap();
+    //         // Set brightness
+    //         rgb.set_brightness(20).unwrap();
+    //
+    //         let left = COLOUR_MAP[same_reading_index % 6];
+    //         let middle = COLOUR_MAP[(same_reading_index + 1) % 6];
+    //         let right = COLOUR_MAP[(same_reading_index + 2) % 6];
+    //
+    //         // Set colours
+    //         rgb.set_pixel(0, left);
+    //         rgb.set_pixel(1, middle);
+    //         rgb.set_pixel(2, right);
+    //
+    //         next_delay = NEAR_DELAY;
+    //
+    //         // Play the next note of the song
+    //         let (note, duration) = HARMONY[note_index];
+    //         // buzzer.tone(&note, duration).unwrap();
+    //
+    //         note_index += 1;
+    //
+    //         let is_at_end_of_song = note_index >= song.len();
+    //
+    //         if is_at_end_of_song {
+    //             // Restart the song
+    //             note_index = 0;
+    //         }
+    //     } else {
+    //         writeln!(uart, "No gaming detected in the vicinity...").unwrap();
+    //         // Set brightness
+    //         rgb.set_brightness(5).unwrap();
+    //
+    //         // Set colours
+    //         rgb.set_pixel(0, (255, 0, 0));
+    //         rgb.set_pixel(1, (0, 255, 0));
+    //         rgb.set_pixel(2, (0, 0, 255));
+    //
+    //         next_delay = FAR_DELAY;
+    //     }
+    //
+    //     rgb.show().unwrap();
+    //
+    //     let readings = temperature_sensor.values().unwrap();
+    //     let altitude = temperature_sensor.altitude(None).unwrap();
+    //     writeln!(
+    //         uart,
+    //         "Temperature: {} Pressure: {} Humidity: {} Altitude: {}",
+    //         readings.temperature, readings.pressure, readings.humidity, altitude
+    //     )
+    //     .unwrap();
+    //
+    //     {
+    //         let mut delay = delay_cell.borrow_mut();
+    //         delay.delay_ms(next_delay);
+    //     }
+    // }
 }
 
 // End of file
