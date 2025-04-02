d60a895d4d2c98edaa4b06d34d650810626f42db
alecbass <alec.bassingthwaighte@gmail.com>
2025-04-02 13:55:05 +1100

    WIP CMake stuff

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+
+int main() {
+    stdio_init_all();
+    while (1) {}
+
+    return 0;
+}
---
+++ test.h
@@ -0,0 +1,7 @@
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+
+int main();
